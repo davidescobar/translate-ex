c2606e8ad4f9020d13579f6198b455a82336985b
J. David Escobar <david.escobar@lendstreet.com>
2016-04-11 23:58:02 -0700

    Updated some commented lines.

--- test/translate_test.exs
+++ test/translate_test.exs
@@ -1,14 +1,14 @@
 defmodule TranslateTest do
-  # use ExUnit.Case
+  use ExUnit.Case
 
   # test "'extract_yaml_value' function returns the value or empty string" do
-  	# Lines in the format 'key: value'
-  #  [ "view: Vista", "view: Vista # This is in spanish" ]
-  #  |> Enum.all? &(assert Translate.extract_yaml_value(&1) == "Vista")
+   # Lines in the format 'key: value'
+   # [ "view: Vista", "view: Vista # This is in spanish" ]
+   # |> Enum.all? &(assert Translate.extract_yaml_value(&1) == "Vista")
 
-    # Lines in the format 'key:' and comments-only lines '# Just a comment on this line.'
-  #  [ "key:", "    sub-categories:      ", "# Just a comment on this line." ]
-  #  |> Enum.all? &(assert Translate.extract_yaml_value(&1) == "")
+   # Lines in the format 'key:' and comments-only lines '# Just a comment on this line.'
+   # [ "key:", "    sub-categories:      ", "# Just a comment on this line." ]
+   # |> Enum.all? &(assert Translate.extract_yaml_value(&1) == "")
   # end
 
 end
